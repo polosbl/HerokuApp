201eaf38bd76f7a4d771859e4bcd21202f84ed03
Alexander Gordeichik <a.gordeichik@invento.by>
2021-05-05 20:02:24 +0300

    Homework7

--- src/test/java/tests/htmlPageTest.java
+++ src/test/java/tests/htmlPageTest.java
@@ -17,7 +17,7 @@ public class htmlPageTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("src/test/resources/testpage.html");
+        driver.get("file:///home/aleksandr/IdeaProjects/HerokuApp/src/test/resources/testpage.html");
         WebElement paragraph = driver.findElement(By.xpath("//*[@data-qa='p1']"));
         String paragrraphText = paragraph.getText();
         Assert.assertEquals(paragrraphText,"Hello World!");
@@ -30,31 +30,31 @@ public class htmlPageTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("src/test/resources/testpage.html");
+        driver.get("file:///home/aleksandr/IdeaProjects/HerokuApp/src/test/resources/testpage.html");
         WebElement button = driver.findElement(By.xpath("//*[@data-qa='submit_button']"));
         Assert.assertTrue(!button.isEnabled());
         driver.quit();
     }
 
     @Test
-    public void checkboxIsUnchecked() {
+    public void checkboxIsUncheckedTest() {
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("src/test/resources/testpage.html");
+        driver.get("file:///home/aleksandr/IdeaProjects/HerokuApp/src/test/resources/testpage.html");
         WebElement checkbox = driver.findElement(By.xpath("//*[@id='1']"));
         Assert.assertTrue(!checkbox.isSelected());
         driver.quit();
     }
 
     @Test
-    public void increaseIntegerTest() {
+    public void inputFieldTest() {
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.get("src/test/resources/testpage.html");
+        driver.get("file:///home/aleksandr/IdeaProjects/HerokuApp/src/test/resources/testpage.html");
         int randomNumber = (int) (Math.random() * 1000);
         WebElement inputField = driver.findElement(By.xpath("//*[@id='2']"));
         inputField.sendKeys(Integer.toString(randomNumber));
